ec1415b2aabf0f4318697eeaadd6823b92feff1f
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-10-05 19:38:51 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,8 +17,8 @@ class Transfer
  
   def execute_transaction
     if @sender.valid? && @receiver.valid? && @sender.balance - @amount > 0
-      @sender.balance = @sender.balance - @amount
-      @receiver.balance = @receiver.balance + @amount
+      @sender.balance -= @amount
+      @receiver.balance += @amount
       @status = "complete"
     else  
       @status = "rejected"
